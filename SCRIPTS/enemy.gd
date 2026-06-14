0366872b22280a61bd2c1d35d0d2a85f6ce80c58
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-06-14 13:00:25 +0200

    Add basic movement in player and enemy

---
+++ SCRIPTS/enemy.gd
@@ -0,0 +1,18 @@
+extends AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.position.x -= 2
+	if self.position.x < -700:
+		self.position.x = 350
+
+func _player_death(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.queue_free()
+	else:
+		print(body.get_groups())
